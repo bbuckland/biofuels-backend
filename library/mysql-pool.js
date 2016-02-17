dea38d5a53e5774bcbfe04fd380360fe7b444634
Bradley Buckland <bradley.buckland@me.com>
2016-02-17 17:45:57 -0500

    Database changes

--- library/mysql-pool.js
+++ library/mysql-pool.js
@@ -13,10 +13,10 @@ var db = 'biofuels';
 
 // grab our environment variables
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-  host = process.env.OPENSHIFT_MYSQL_DB_HOST;
-  port = process.env.OPENSHIFT_MYSQL_DB_PORT;
-  user = process.env.OPENSHIFT_MYSQL_DB_USERNAME;
-  pass = process.env.OPENSHIFT_MYSQL_DB_PASSWORD;
+  host = process.env.OPENSHIFT_MYSQLDB_DB_HOST;
+  port = process.env.OPENSHIFT_MYSQLDB_DB_PORT;
+  user = process.env.OPENSHIFT_MYSQLDB_DB_USERNAME;
+  pass = process.env.OPENSHIFT_MYSQLDB_DB_PASSWORD;
 } else {
   host = 'localhost';
   port = '3306';
