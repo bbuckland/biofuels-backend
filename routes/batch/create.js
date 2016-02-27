6dce6b0d96c5c34167f155e8999a8ceedfb6679f
Joshua <joshua.olu.adebayo@gmail.com>
2016-02-26 23:41:28 -0500

    add create batch and samples, does not yet create replicates

--- routes/batch/create.js
+++ routes/batch/create.js
@@ -1,4 +1,7 @@
 /**
+ * Created by joa3894 on 2/26/2016.
+ */
+/**
  * Created by joa3894 on 2/22/2016.
  */
 /**
@@ -12,7 +15,7 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var Ajv = require('ajv');
-var authSchema = require('../../json/customer/create.json');
+var authSchema = require('../../json/batch/create.json');
 var db = require('../../library/mysql-pool.js');
 
 //init express router
@@ -49,24 +52,37 @@ router.post('/', function (req, res) {
         });
     }
 
-    var sql = 'INSERT INTO `bio_customers` (`customer_name`, `customer_code`, `contact_name1`, `contact_name2`, `billing_name`, `address_1`, `address_2`, `city`, `state`, `postal_code`, `phone_number`, `email`) VALUES (? , ? , ? , ? , ?, ? , ? , ? , ? , ?, ?, ?)';
-    var sqlParams = [params.customer_name, params.customer_code, params.contact_name1, params.contact_name2, params.billing_name, params.address_1, params.address_2, params.city, params.state, params.postal_code, params.phone_number, params.email ];
 
-    var sql1 = 'SELECT * FROM bio_customers WHERE customer_code = ?';
+
+    var sql1 = 'SELECT `id` FROM bio_customers WHERE customer_code = ?';
     var sqlParams1 = [params.customer_code];
 
     db.query(sql1, sqlParams1).then(function (data) {
-        if (data.length < 1) {
-            db.query(sql, sqlParams).then(function (data) {
+        if (data.length != 1) {
+            res.status(401);
+            return res.json({
+                code: 401,
+                error: "That customer was not found"
+            });
+        }
+        var response = data[0];
 
+        var cust_id = response.id;
+        console.log(cust_id);
+        var sql = 'INSERT INTO `bio_batches`(`customer_id`, `received_date`, `shipped_date`, `sample_purpose`, `sample_storage_location`) VALUES (? , ? , ? , ? , ?)';
+        var sqlParams = [cust_id, params.received_date, params.shipped_date, params.sample_purpose, params.sample_storage_location];
 
+        db.query(sql, sqlParams).then(function (data) {
 
-                var insertedID = data.insertId;
-                for (var count=0; count<params.po_num.length; count++)
-                {
-                    var sql2 = 'INSERT INTO `bio_po_numbers` (`customer_id`, `po_number`) VALUES (? , ? )';
-                    var sqlParams2 = [insertedID, params.po_num[count]];
-                    db.query(sql2, sqlParams2).then(function () {
+            var batch_id = data.insertId;
+            for (var count=0; count<params.sample_data.length; count++)
+            {
+                var sql2 = 'INSERT INTO `bio_samples`( `batch_id`, `type`, `name`, `description`, `species_id`, `container_type`, `collection_date`, `is_limited`) VALUES (? , ?, ? , ? , ? , ? , ?, ? )';
+                var sqlParams2 = [batch_id, params.sample_data[count].sample_type, params.sample_data[count].sample_name, params.sample_data[count].sample_description, params.sample_data[count].species_id, params.sample_data[count].container_type, params.sample_data[count].collection_date, params.sample_data[count].is_limited];
+                db.query(sql2, sqlParams2).then(function () {
+
+                    console.log("entered sample" + count + "for the batch");
+                    //setup our response
 
 
                     }).catch(function (err) {
@@ -74,43 +90,34 @@ router.post('/', function (req, res) {
                         res.status(500);
                         return res.json({
                             code: 500,
-                            error: 'Uh oh! We can\'t even!'
+                            error: 'Uh oh! We can\'t even!3'
                         });
                     });
 
                 }
+            var resp = {
+                status: 'Ok'
+            };
+
+            //send down our response
+            res.json(resp);
 
             }).catch(function (err) {
                 console.error('MySQL: ', err);
                 res.status(500);
                 return res.json({
                     code: 500,
-                    error: 'Uh oh! We can\'t even!'
+                    error: 'Uh oh! We can\'t even2!'
                 });
             });
-        }
-        else {
-            console.error('Customer ' + params.customer_name + ' already exists');
-            res.status(409);
-            return res.json({
-                code: 409,
-                error: "Conflict: Customer already exists"
-            });
-        }
 
-        //setup our response
-        var resp = {
-            status: 'Ok'
-        };
 
-        //send down our response
-        res.json(resp);
     }).catch(function (err) {
         console.error('MySQL: ', err);
         res.status(500);
         return res.json({
             code: 500,
-            error: 'Uh oh! We can\'t even!'
+            error: 'Uh oh! We can\'t even1!'
         });
     });
 
